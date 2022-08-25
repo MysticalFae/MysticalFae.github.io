d51a1e920e1c7419efd6eb63dccde4a9b2030ef1
MysticalFae <sky533539@gmail.com>
2022-08-25 13:05:31 -0500

    added lesson 2

---
+++ Unit5Con/if.js
@@ -0,0 +1,31 @@
+console.log("works");
+
+//1st conditional statment
+//person = 18 or older?
+let age = 20;
+
+if (age >= 18)
+{
+    console.log("You are old enough to vote");
+}
+
+console.log("Thanks for check your age");
+
+//if you can go to mall
+//grades >= 90
+
+let csGrade = 95;
+
+if (csGrade >= 90)
+{
+    console.log("You can go to the mall");
+}
+
+//if age < 16, can't drive
+
+age = 14; //defined above
+
+if (age < 16)
+{
+    console.log("You can't drive!");
+}
