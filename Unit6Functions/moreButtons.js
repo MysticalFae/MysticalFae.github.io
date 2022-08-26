a79b35fff9e6bb00742cfc81ece76752f288ebab
MysticalFae <sky533539@gmail.com>
2022-08-26 16:08:42 -0500

    added quadratic formula w/ imaginary!!

--- Unit6Functions/moreButtons.js
+++ Unit6Functions/moreButtons.js
@@ -16,6 +16,15 @@ let elCirR = document.getElementById("cir-r");
 let elCirA = document.getElementById("cir-area");
 let elCirCalc = document.getElementById("calc-cir-area");
 
+//Quadratic elements
+let elA = document.getElementById("a");
+let elB = document.getElementById("b");
+let elC = document.getElementById("c");
+let elResultSub = document.getElementById("quad-result-sub");
+let elResultAdd = document.getElementById("quad-result-add");
+let elCalcResult = document.getElementById("quad-calc");
+
+
 
 // rect button/calc
 elCalcR.onclick = function() 
@@ -40,6 +49,7 @@ elCalcV.onclick = function()
 
 }
 
+
 // Circle button/calc
 
 elCirCalc.onclick = function()
@@ -47,4 +57,56 @@ elCirCalc.onclick = function()
     let cirR = elCirR.value;
     let cirA = Math.PI * cirR**2;
     elCirA.value = cirA;
+}
+
+
+//quadratic button/calc (no imaginary stuff) -- didn't want to lose it
+
+// elCalcResult.onclick = function()
+// {
+//     let a = elA.value;
+//     let b = elB.value;
+//     let c = elC.value;
+
+//     let resultSub = (-(b) - Math.sqrt((b**2) -4 * a * c)) / (2 * a);
+//     let resultAdd = (-(b) + Math.sqrt((b**2) -4 * a * c)) / (2 * a);
+//     resultSub = resultSub.toFixed(3);
+//     resultAdd = resultAdd.toFixed(3);
+
+//     elResultSub.value = resultSub;
+//     elResultAdd.value = resultAdd;
+
+elCalcResult.onclick = function()
+{
+    let a = elA.value;
+    let b = elB.value;
+    let c = elC.value;
+
+    let inside = (b**2) - 4 * a * c;
+
+    if (inside < 0)
+    {
+        newInside = -(inside);
+        console.log(newInside);
+        let insidePart = Math.sqrt(newInside) / (2 * a);
+        let outsidePart = -b / (2 * a);
+        outsidePart = outsidePart.toFixed(3);
+        insidePart = insidePart.toFixed(3);
+        let resultSub = outsidePart + " - " + insidePart + "i";
+        let resultAdd = outsidePart + " + " + insidePart + "i";
+        // let resultSub = (-(b) - Math.sqrt(newInside) / (2 * a));
+        // let resultAdd = (-(b) + Math.sqrt(newInside) / (2 * a));
+        elResultSub.value = resultSub;
+        elResultAdd.value = resultAdd;
+    } else {
+
+    let resultSub = (-(b) - Math.sqrt((b**2) -4 * a * c)) / (2 * a);
+    let resultAdd = (-(b) + Math.sqrt((b**2) -4 * a * c)) / (2 * a);
+    resultSub = resultSub.toFixed(3);
+    resultAdd = resultAdd.toFixed(3);
+    elResultSub.value = resultSub;
+    elResultAdd.value = resultAdd;
+    }
+    
+    
 }
